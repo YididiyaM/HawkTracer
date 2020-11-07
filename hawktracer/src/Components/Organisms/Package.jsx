7752e1e6906f321322b04aab48820bfbdc521881
Yididiya M. Gudisa <yididiyamulatu@gmail.com>
2020-11-07 16:45:03 +0100

    accidentally deleted all files from previus branch

---
+++ hawktracer/src/Components/Organisms/Package.jsx
@@ -0,0 +1,22 @@
+// React core
+import React from "react";
+
+export default function Package({data}) {
+    const {
+        id, 
+        eta,
+        location_name,
+        sender,
+        status,
+        parcel_id}=data
+
+  return (
+    <article className="package">
+        <h4> Parcel id: {parcel_id}</h4>
+  <p>Estimated time of arrival{eta}</p>
+  <p>Pick up location:{location_name}</p>
+  <p>Sender:{sender}</p>
+  <p>Status:{status}</p>
+    </article>
+  );
+}
