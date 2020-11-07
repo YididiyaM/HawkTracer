f363643652e57ef5c03a2197bd729eeccb6eb67c
Yididiya M. Gudisa <yididiyamulatu@gmail.com>
2020-11-07 18:43:39 +0100

    search function that goes nowhere

--- hawktracer/src/Components/Templates/HomePage.jsx
+++ hawktracer/src/Components/Templates/HomePage.jsx
@@ -1,19 +1,27 @@
 //React core
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 
 // Component
 import Header from "../Organisms/Header.jsx";
-import AllPackages from "../Templates/AllPackages.jsx"
 
 export default function HomePage() {
+
+  //reactive data
+  const [query, setQuery] = useState("");
     return (
     <div className="home-page">
         <Header />
          <div className="search-bar">
-        <input placeholder="Enter the four-digit package Id" />
-        <Link to="/ResultsPage">Search</Link>
+        
+        <input placeholder="Enter the four-digit package Id" 
+        value ={query} 
+        onChange ={(event)=> setQuery(event.target.value)}
+        />
+        <Link to={`/ResultsPage/${query}`}>Search </Link>
+        
+        
         <Link to ="/AllPackages">See all packages</Link>
         </div>
      
